1d1de8a43e05675522a5aa5f9b0f2f48f0ecae90
caipxne <151857246+caipxne@users.noreply.github.com>
2024-01-03 16:26:13 +0700

    Update setv6.sh

--- setv6.sh
+++ setv6.sh
@@ -13,29 +13,7 @@ sysctl -p
 IPC=$(curl -4 -s icanhazip.com | cut -d"." -f3)
 IPD=$(curl -4 -s icanhazip.com | cut -d"." -f4)
 ##
-if [ $IPC == 4 ]
-then
-   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
-	IPV6INIT=yes
-	IPV6_AUTOCONF=no
-	IPV6_DEFROUTE=yes
-	IPV6_FAILURE_FATAL=no
-	IPV6_ADDR_GEN_MODE=stable-privacy
-	IPV6ADDR=2403:6a40:2:7000::$IPD:0000/56
-	IPV6_DEFAULTGW=2403:6a40:2:7000::1
-	EOF
-elif [ $IPC == 5 ]
-then
-   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
-	IPV6INIT=yes
-	IPV6_AUTOCONF=no
-	IPV6_DEFROUTE=yes
-	IPV6_FAILURE_FATAL=no
-	IPV6_ADDR_GEN_MODE=stable-privacy
-	IPV6ADDR=2403:6a40:0:41::$IPD:0000/64
-	IPV6_DEFAULTGW=2403:6a40:0:41::1
-	EOF
-elif [ $IPC == 143 ]
+if [ $IPC == 143 ]
 then
    tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
 	IPV6INIT=yes
@@ -46,27 +24,7 @@ then
 	IPV6ADDR=2403:6a40:2:4100::$IPD:0000/56
 	IPV6_DEFAULTGW=2403:6a40:2:4100::1
 	EOF
-elif [ $IPC == 244 ]
-then
-   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
-	IPV6INIT=yes
-	IPV6_AUTOCONF=no
-	IPV6_DEFROUTE=yes
-	IPV6_FAILURE_FATAL=no
-	IPV6_ADDR_GEN_MODE=stable-privacy
-	IPV6ADDR=2403:6a40:2000:244::$IPD:0000/64
-	IPV6_DEFAULTGW=2403:6a40:2000:244::1
-	EOF
-else
-	tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
-	IPV6INIT=yes
-	IPV6_AUTOCONF=no
-	IPV6_DEFROUTE=yes
-	IPV6_FAILURE_FATAL=no
-	IPV6_ADDR_GEN_MODE=stable-privacy
-	IPV6ADDR=2403:6a40:0:$IPC::$IPD:0000/64
-	IPV6_DEFAULTGW=2403:6a40:0:$IPC::1
-	EOF
+
 fi
 
 service network restart
